9fe43699cb223596eb328f9390dfb81c59c83e35
Vikalp.Sahni <vikalp.Sahni@ibibogroup.com>
2013-05-25 23:36:49 +0530

    remvoved one line testing changes

--- pyflakes/messages.py
+++ pyflakes/messages.py
@@ -89,6 +89,7 @@ class UnusedVariable(Message):
     Indicates that a variable has been explicity assigned to but not actually
     used.
     """
+
     message = 'local variable %r is assigned to but never used'
     def __init__(self, filename, loc, names):
         Message.__init__(self, filename, loc)
